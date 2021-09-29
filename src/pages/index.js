34aed081fdee88fef1505a14dc81020d9bbc23dd
tomas <tomas.tesfamariam.berhe@student.gildeopleidingen.nl>
2021-09-29 15:13:26 +0200

    head updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,13 @@ import Technologies from "../components/Technologies/Technologies";
 import Timeline from "../components/TimeLine/TimeLine";
 import { Layout } from "../layout/Layout";
 import { Section } from "../styles/GlobalComponents";
-
+import Head from "next/head"
 const Home = () => {
   return (
     <Layout>
+      <Head>
+      <title>my portfolio</title>
+        </Head>
       <Section grid>
         <Hero />
         <BgAnimation />
